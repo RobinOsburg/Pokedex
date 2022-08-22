d95184ee3f89692a06792819ff3e0a2c466d37a8
Robin Osburg <Robin.osburg@web.de>
2022-08-22 12:51:33 +0200

    added abbilities and numbers

--- script.js
+++ script.js
@@ -16,5 +16,24 @@ async function loadPokemon(){
 
 function renderPokemonInfo(){
     document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokeImg').src = currentPokemon['sprites']['front_default']
+    document.getElementById('pokeImg').src = currentPokemon['sprites']['front_default'];
+    document.getElementById('hp').innerHTML += currentPokemon['stats']['0']['stat']['name'];
+    document.getElementById('number1').innerHTML += currentPokemon['stats']['0']['base_stat'];
+    document.getElementById('attack').innerHTML += currentPokemon['stats']['1']['stat']['name'];
+    document.getElementById('number2').innerHTML += currentPokemon['stats']['1']['base_stat']; 
+    document.getElementById('defense').innerHTML += currentPokemon['stats']['2']['stat']['name'];
+    document.getElementById('number3').innerHTML += currentPokemon['stats']['2']['base_stat']; 
+    document.getElementById('special_attack').innerHTML += currentPokemon['stats']['3']['stat']['name']; 
+    document.getElementById('number4').innerHTML += currentPokemon['stats']['3']['base_stat'];
+    document.getElementById('special_defense').innerHTML += currentPokemon['stats']['4']['stat']['name']; 
+    document.getElementById('number5').innerHTML += currentPokemon['stats']['4']['base_stat'];
+    document.getElementById('speed').innerHTML += currentPokemon['stats']['5']['stat']['name'] ;
+    document.getElementById('number6').innerHTML += currentPokemon['stats']['5']['base_stat'];
+
+    
+    
+
+    
+
 }
+
