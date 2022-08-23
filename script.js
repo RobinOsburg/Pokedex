51a3e1508d86ac7f24e51317526a3b4c4c61eaba
Robin Osburg <Robin.osburg@web.de>
2022-08-23 10:47:19 +0200

    added variable barNumbers

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let currentPokemon;
 
 
-
-
 async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
     let response = await fetch(url);
@@ -11,7 +9,6 @@ async function loadPokemon() {
     console.log('Loaded pokemon', currentPokemon);
 
     renderPokemonInfo();
-
 }
 
 function renderPokemonInfo() {
@@ -30,7 +27,8 @@ function renderPokemonInfo() {
     document.getElementById('speed').innerHTML += currentPokemon['stats']['5']['stat']['name'];
     document.getElementById('number6').innerHTML += currentPokemon['stats']['5']['base_stat'];
 
-    showMoreInfo()
+    showMoreInfo();
+    showBar();
 }
 
 function showMoreInfo() {
@@ -41,7 +39,12 @@ function showMoreInfo() {
 }
 
 function showBar() {
-    document.getElementById('bar_1').style.width = currentPokemon['stats']['0']['base_stat'];
+    document.getElementById('bar_1').style = `width: ${currentPokemon['stats']['0']['base_stat']}%`;
+    document.getElementById('bar_2').style = `width: ${currentPokemon['stats']['1']['base_stat']}%`;
+    document.getElementById('bar_3').style = `width: ${currentPokemon['stats']['2']['base_stat']}%`;
+    document.getElementById('bar_4').style = `width: ${currentPokemon['stats']['3']['base_stat']}%`;
+    document.getElementById('bar_5').style = `width: ${currentPokemon['stats']['4']['base_stat']}%`;
+    document.getElementById('bar_6').style = `width: ${currentPokemon['stats']['5']['base_stat']}%`;
 }
 
 
